6afff8386f93874da53c3779d0da1ae71a559418
Wojciech Dzimira <wojciech.dzimira@gmail.com>
2026-03-05 13:06:06 +0100

    Add if __name__ == __main__

--- kalkulator.py
+++ kalkulator.py
@@ -30,6 +30,9 @@ def int_input(text):
             print("to nie jest dodatnia liczba całkowita, wprowadz jeszcze raz.")
 
 def operation_components():
+    """
+    Funkcja pobiera 2 liczby i zwraca jew
+    """
     number_1 = number_input("Podaj składnik 1. ")
     number_2 = number_input("Podaj składnik 2. ")
     return number_1, number_2
@@ -100,29 +103,29 @@ def division():
 
 
 
+if __name__ == "__main__":
+    while True:
+        print(
+                "\n"
+                "Podaj działanie, posługując się odpowiednią liczbą::\n"
+                "  1  Dodawanie\n"
+                "  2  Odejmowanie\n"
+                "  3  Mnożenie\n"
+                "  4  Dzielenie\n"
+                )
 
-while True:
-    print(
-            "\n"
-            "Podaj działanie, posługując się odpowiednią liczbą::\n"
-            "  1  Dodawanie\n"
-            "  2  Odejmowanie\n"
-            "  3  Mnożenie\n"
-            "  4  Dzielenie\n"
-            )
-
-    operation = operation_choosing()
-    if operation == 1:
-        addition()  
-    elif operation == 2:
-        subtraction()
-    elif operation == 3:
-        multiplication()
-    elif operation == 4:
-        division()
+        operation = operation_choosing()
+        if operation == 1:
+            addition()  
+        elif operation == 2:
+            subtraction()
+        elif operation == 3:
+            multiplication()
+        elif operation == 4:
+            division()
+        
+        next = input("chcesz wykonać następne działanie? wpisz T, chcesz zakończyć wpisz cokolwiek ")
+        if next.lower() != "t":
+            break
     
-    next = input("chcesz wykonać następne działanie? wpisz T, chcesz zakończyć wpisz cokolwiek ")
-    if next.lower() != "t":
-        break
-   
 
